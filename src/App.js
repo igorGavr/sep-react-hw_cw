53676a0a60040ee2687defba83c16eb54385a1e6
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-01-14 21:15:51 +0200

    hw2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import Users from "./components/Users/Users";
+import UserDetails from "./components/UsersDetails/UserDetails";
+import Posts from "./components/Posts/Posts";
+import css from './App.module.css';
+import {useState} from "react";
+
 const App = () => {
+    const [user, setUser] = useState(null);
+    const [userId, setUserId] = useState(null);
+
+    const getUser = (user) => {
+      setUser(user)
+        setUserId(null)
+    }
+
+    const getUserId = (id) => {
+        setUserId(id)
+    }
+
     return (
         <div>
-
+            <div className={css.wrap}>
+                <Users getUser={getUser}/>
+                {user && <UserDetails user={user} getUserId={getUserId}/>}
+            </div>
+            {userId && <Posts userId={userId}/>}
         </div>
     );
 };
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,17 @@
+import Post from "../Post/Post";
+import {useEffect, useState} from "react";
+import {postService} from "../../services/post.service";
+
+const Posts = ({userId}) => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        postService.getByUserId(userId).then(value => setPosts(value))
+    }, [userId])
+    return (
+        <div>
+            {posts.map(post => <Post key={post.id} post={post}/>)}
+        </div>
+    );
+};
+
+export default Posts;
